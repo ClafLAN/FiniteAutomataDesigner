0a5387f96bbf85ba6e1d6208acb9451b9a39a840
Charles Allen Schultz II <>
2015-10-01 16:56:57 -0400

    Started expanding UI branch; did some minor manipulations of files.

---
+++ src/edu/claflin/fad/ui/MainWindow.java
@@ -0,0 +1,13 @@
+package edu.claflin.fad.ui;
+
+import javax.swing.JFrame;
+
+public class MainWindow extends JFrame {
+    
+        public MainWindow() {
+            // TODO - construct Main window
+        }
+        private void init() {
+            // Initialize window components.
+        }
+}
